4d24b6c8c2118dc8341d29858a38587e7b697a98
Jan Viilma <janviilma@hotmail.com>
2019-04-22 22:33:52 +0300

    Initial commit

---
+++ elements-api/src/main/java/com/elements/elementsapi/ElementsApiApplication.java
@@ -0,0 +1,13 @@
+package com.elements.elementsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElementsApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElementsApiApplication.class, args);
+    }
+
+}
