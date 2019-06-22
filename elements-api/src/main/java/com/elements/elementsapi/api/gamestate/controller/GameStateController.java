414d4ea8ace018268f848cd45c9c22603bb52def
PrimeBurgerito <burgerito@protonmail.com>
2019-06-22 15:34:53 +0300

    game state

--- elements-api/src/main/java/com/elements/elementsapi/api/gamestate/controller/GameStateController.java
+++ elements-api/src/main/java/com/elements/elementsapi/api/gamestate/controller/GameStateController.java
@@ -4,6 +4,7 @@ import com.elements.elementsapi.api.gamestate.service.GameStateService;
 import com.elements.elementsapi.api.gamestate.service.resource.GameStateDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,7 +16,7 @@ public class GameStateController {
     private final GameStateService gameStateService;
 
     @PostMapping
-    public boolean create(GameStateDto gameStateDto) {
+    public boolean create(@RequestBody GameStateDto gameStateDto) {
         return gameStateService.create(gameStateDto);
     }
 }
--- elements-api/src/main/java/com/elements/elementsapi/api/gamestate/service/GameStateService.java
+++ elements-api/src/main/java/com/elements/elementsapi/api/gamestate/service/GameStateService.java
@@ -3,12 +3,14 @@ package com.elements.elementsapi.api.gamestate.service;
 import com.elements.elementsapi.api.charactertemplate.repository.CharacterTemplateRepository;
 import com.elements.elementsapi.api.gamestate.repository.GameStateRepository;
 import com.elements.elementsapi.api.gamestate.service.resource.GameStateDto;
+import com.elements.elementsapi.api.user.UserRepository;
 import com.elements.elementscommon.domain.user.User;
 import com.elements.elementsdomain.character.CharacterTemplate;
 import com.elements.elementsdomain.gamestate.CharacterStatistics;
 import com.elements.elementsdomain.gamestate.GameState;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -18,10 +20,13 @@ import org.springframework.transaction.annotation.Transactional;
 public class GameStateService {
 
     private final GameStateRepository gameStateRepository;
+    private final UserRepository userRepository;
     private final CharacterTemplateRepository characterTemplateRepository;
 
     public boolean create(GameStateDto gameStateDto) {
-        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Username not found: " + username));
         CharacterTemplate characterTemplate = characterTemplateRepository
                 .findById(gameStateDto.getCharacterTemplateId())
                 .orElseThrow(() -> new RuntimeException("Character template not found"));
