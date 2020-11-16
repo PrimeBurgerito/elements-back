529f4676fa8d705a230fd894c90b76200e78ede9
PrimeBurgerito <burgerito@protonmail.com>
2020-11-16 21:50:05 +0200

    add google drive support

--- elements-api/src/main/java/com/elements/elementsapi/ElementsApiApplication.java
+++ elements-api/src/main/java/com/elements/elementsapi/ElementsApiApplication.java
@@ -3,9 +3,13 @@ package com.elements.elementsapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoReactiveAutoConfiguration;
 
 
-@SpringBootApplication(scanBasePackages = {"com.elements"}, exclude = {MongoAutoConfiguration.class})
+@SpringBootApplication(
+        scanBasePackages = {"com.elements"},
+        exclude = {MongoAutoConfiguration.class, MongoReactiveAutoConfiguration.class}
+)
 public class ElementsApiApplication {
     public static void main(String[] args) {
         SpringApplication.run(ElementsApiApplication.class, args);
