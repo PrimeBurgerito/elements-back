014db3979f6cfb2d2d9ce1bdbf94963293666c09
PrimeBurgerito <burgerito@protonmail.com>
2020-11-21 14:16:37 +0200

    try

--- elements-api/src/main/java/com/elements/elementsapi/ElementsApiApplication.java
+++ elements-api/src/main/java/com/elements/elementsapi/ElementsApiApplication.java
@@ -2,14 +2,9 @@ package com.elements.elementsapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
-import org.springframework.boot.autoconfigure.mongo.MongoReactiveAutoConfiguration;
 
 
-@SpringBootApplication(
-        scanBasePackages = {"com.elements"},
-        exclude = {MongoAutoConfiguration.class, MongoReactiveAutoConfiguration.class}
-)
+@SpringBootApplication(scanBasePackages = {"com.elements"})
 public class ElementsApiApplication {
     public static void main(String[] args) {
         SpringApplication.run(ElementsApiApplication.class, args);
